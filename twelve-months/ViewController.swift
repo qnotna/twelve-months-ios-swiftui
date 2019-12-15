8b5f1739fbe3f46f2044b9ac74abd52c29a7a430
Anton <qau@gmx.net>
2019-12-15 23:00:47 +0100

    Add xcode project

---
+++ twelve-months/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  twelve-months
+//
+//  Created by Anton Quietzsch on 15.12.19.
+//  Copyright © 2019 Anton Quietzsch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
